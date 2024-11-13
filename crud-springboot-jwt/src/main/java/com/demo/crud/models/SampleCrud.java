2622aaac73cbda7b3635b50bb3999eb532617f45
n-hassan01 <naimul.hassan.se@gmail.com>
2024-11-13 18:02:18 +0600

    bismillahir rahmanir rahim

---
+++ crud-springboot-jwt/src/main/java/com/demo/crud/models/SampleCrud.java
@@ -0,0 +1,56 @@
+package com.demo.crud.models;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Component
+@Entity
+public class SampleCrud {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private int id;
+	private String name;
+	private String description;
+	private Date creationDate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+}
